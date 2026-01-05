7b9490ea0a92186fcf399af43c71611bea48f4b9
MJ52234 <mateo.jaho1@cit.edu.al>
2026-01-05 12:49:09 +0100

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,23 @@
+length = 8
+width  = 4
+
+perimeter = 2 * (length + width)
+area = (length * width)
+
+print("Perimeter of the rectangle",perimeter)
+print("Area of the rectangle",area)
+
+
+
+
+
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+
+
+celsius_to_fahrenheit = (celsius * 9 / 5) + 32
+fahrenheit_to_celsius = (fahrenheit - 32) * 5 / 9
+
+
+print("Celsius to Fahrenheit:", celsius_to_fahrenheit)
+print("Fahrenheit to Celsius:", fahrenheit_to_celsius)
